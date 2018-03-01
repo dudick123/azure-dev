e2e98a5f7dbe4ec35d219cd05ab6655d01680b08
dudick123 <brian.dudick@outlook.com>
2018-02-28 19:44:25 -0500

    Update az-cli-create-vm-jenkins-docker-ubuntu.sh

--- jenkins-docker-ubuntu/az-cli-create-vm-jenkins-docker-ubuntu.sh
+++ jenkins-docker-ubuntu/az-cli-create-vm-jenkins-docker-ubuntu.sh
@@ -7,8 +7,8 @@ az group create --name my-resource-group --location southcentralus
 az vm create --resource-group my-resource-group --name jenkins-docker-ubuntu --image UbuntuLTS --admin-username azureuser --generate-ssh-keys
 
 # Open port 80 to allow web traffic to host.
-az vm open-port --port 80 --resource-group my-resource-group --name jenkins-docker-ubuntu
-az vm open-port --port 8080 --resource-group my-resource-group --name jenkins-docker-ubuntu
+az vm open-port --port 80 --resource-group my-resource-group --name jenkins-docker-ubuntu --priority 100
+az vm open-port --port 8080 --resource-group my-resource-group --name jenkins-docker-ubuntu --priority 101
 
 # Start a CustomScript extension to use a simple bash script to update, download and install WordPress and MySQL 
 az vm extension set \
